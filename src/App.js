a878d482bae53f3627466e59e6a49f3b6a47587a
Oliver Schmidt <kontakt@webkreation.de>
2017-03-18 18:09:25 +0100

    remove css, add styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
+import styled, { keyframes } from 'styled-components';
 import logo from './reactStuttgartLogo.jpg';
-import './App.css';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 200px;
+  padding: 20px;
+  color: white;
+`;
+
+const AppIntro = styled.p`
+  font-size: large;
+`;
+
+const rotate360 = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const Logo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 80px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppWrapper>
+        <AppHeader>
+          <Logo src={logo} alt="logo" />
           <h2>Welcome to ReactStuttgart</h2>
-        </div>
-        <p className="App-intro">
+        </AppHeader>
+        <AppIntro>
           6. Meetup - React Stuttgart @ ITM
-        </p>
-        <p className="App-intro">
+        </AppIntro>
+        <AppIntro>
           Freitag, 24. März 2017 - 18:30
-        </p>
-      </div>
+        </AppIntro>
+      </AppWrapper>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'normalize.css';
-import './index.css';
+import { injectGlobal } from 'styled-components';
+import App from './App';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+`
 
 ReactDOM.render(
   <App />,
