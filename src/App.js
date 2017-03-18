4841dd6a6a0b650d1895491acdb87a4b8ae1f90e
Oliver Schmidt <kontakt@webkreation.de>
2017-03-18 17:30:33 +0100

    Add normalize.css, change content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './reactStuttgartLogo.jpg';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to ReactStuttgart</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          6. Meetup - React Stuttgart @ ITM
+        </p>
+        <p className="App-intro">
+          Freitag, 24. März 2017 - 18:30
         </p>
       </div>
     );
